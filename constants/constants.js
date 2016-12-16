3438a8a063e8efa610429d7f3a1ec968f0781494
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-12-16 08:44:18 -0500

    Make endPoint test for createToken.

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,9 @@
 module.exports = {
     responseCodes: {
         created: 201
+    },
+    requestURL: 'https://localhost:3000',
+    endPoints: {
+        createTokenUrl: '/api/v1/createToken'
     }
-}
+};
--- test/unit-tests/createToken.test.js
+++
@@ -1,5 +0,0 @@
-const test = require('ava');
-
-test('createToken should return jwt', t => {
-    t.pass();
-});
