cde902dded7d070fc7f8c069a7b12993e4a44666
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-02-05 18:36:12 -0500

    Fix issue with dotenv path in travis.

--- config/config.js
+++ config/config.js
@@ -1,3 +1,11 @@
+'use strict';
+
+const {
+  join
+} = require('path');
+
 module.exports = {
-  dotEnvConfig: require('dotenv').config()
+  dotEnvConfig: require('dotenv').config({
+    path: join(__dirname, '../.env')
+  })
 };
